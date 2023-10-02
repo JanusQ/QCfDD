9c16b32fc7eab3b8d8c459b6f33a0ff49c99441d
AC <chen0618_2@163.com>
2023-10-02 13:18:16 +0800

    some heartbreaking result

--- zne_implementation.py
+++ zne_implementation.py
@@ -69,7 +69,7 @@ vqe_kwargs = {
         "init_last": False,
         "HF_bitstring": HF_bitstring,
         #ac add:
-        "readout_error_mitigation": True,
+        "readout_error_mitigation": False,
     }
 
 #tcircs = all_transpiled_vqe_circuits(n_qubits, parameters, paulis, backend, **kwargs)
@@ -106,7 +106,7 @@ def qiskit_executor(circuit: qiskit.QuantumCircuit, shots: int = 5000) -> float:
     return expectation_val
 
 #debug, ac, only for test
-'''pauli=paulis[3]  #先算第一个电路 注意为2时是有问题的！！！ac
+'''pauli=paulis[3]  #先算第一个电路 注意为2时是有问题的!!! ac
 imp_circuit=vqe_circuit(n_qubits, parameters, pauli, **vqe_kwargs)
 
 unmitigated=qiskit_executor(imp_circuit)
@@ -141,21 +141,27 @@ def compute_expectations_zne(n_qubits, parameters,  noise_backend,paulis, shots,
     expectations=[]
     unmitigated_expectations=[]
     for pauli in paulis:
-        imp_circuit=vqe_circuit(n_qubits, parameters, pauli, **vqe_kwargs)
-        unmitigated=qiskit_executor(imp_circuit)
-        mitigated = zne.execute_with_zne(imp_circuit, qiskit_executor)
-        expectations.append(mitigated)
+        if pauli == len(pauli)*'I':
+            expectations.append(1.0)
+        else:
+            imp_circuit=vqe_circuit(n_qubits, parameters, pauli, **vqe_kwargs)
+            unmitigated=qiskit_executor(imp_circuit)
+            mitigated = zne.execute_with_zne(imp_circuit, qiskit_executor)
+            expectations.append(mitigated)
+            unmitigated_expectations.append(unmitigated)
     #unmitigated_expectations is for debugging only, ac
     return expectations,unmitigated_expectations
 
 important_terms=find_imp_terms(coeffs,threshold=1) #format:[1,8,13,...] indexes
+#important_terms.pop(0) #ac: temporary,debug
 print('important_terms: ',important_terms) #debug, ac
 imp_coeffs,imp_paulis,trivial_coeffs,trivial_paulis=cut_paulis(important_terms,coeffs,paulis)
-print('imp_coeffs: ',imp_coeffs,'/n imp_paulis',imp_paulis,'/n trivial_coeffs',trivial_coeffs)
+print('imp_coeffs: ',imp_coeffs,'\n imp_paulis',imp_paulis)
+#print('\n trivial_coeffs',trivial_coeffs)
 
 #, mitigator=mitigator 暂时删去
-trivial_expectations = compute_expectations(n_qubits, parameters, noise_backend=noise_backend,paulis=trivial_paulis, shots=shots, backend=sys_backend, mode="device_execution", **vqe_kwargs)
 imp_expectations, _=compute_expectations_zne(n_qubits, parameters, noise_backend=noise_backend,paulis=imp_paulis, shots=shots, backend=sys_backend, mode="device_execution", **vqe_kwargs)
+trivial_expectations = compute_expectations(n_qubits, parameters, noise_backend=noise_backend,paulis=trivial_paulis, shots=shots, backend=sys_backend, mode="device_execution", **vqe_kwargs)
 
 #debug: print, ac
 print('imp_expectations: ',imp_expectations)
@@ -163,3 +169,87 @@ print('unmitigated_imp_expectations:', _)
 print('trivial_expectations: ',trivial_expectations)
 imp_energy = np.inner(imp_coeffs, imp_expectations)
 trivial_energy=np.inner(trivial_coeffs,trivial_expectations)
+print('important energy:',imp_energy,'trivial energy:',trivial_energy,'total energy:',imp_energy+trivial_energy)
+
+def vqe_zne(n_qubits, parameters, coeffs, paulis, coeffs, shots , backend, mode="device_execution", loss_filename=None, params_filename=None,mitigator=None,noise_backend=None, **vqe_kwargs):
+    """
+    Compute the VQE loss/energy.
+    n_qubits (Int): Number of qubits in circuit.
+    parameters (Iterable[Float]): VQE parameters.ä
+    coeffs (Iterable[Float]): Pauli coefficients in Hamiltonian.
+    loss_filename (String): Path to save file for VQE loss/energy.
+    params_filename (String): Path to save file for VQE parameters.
+    kwargs (Dict): All the arguments that need to be passed on to the next function calls.
+    backend: pay attention that this is the system backend
+    Returns:
+    (Float) VQE energy. 
+    """
+    #print('vqe start')  #debug,ac
+    start = timer()
+    expectations = compute_expectations(n_qubits, parameters,mitigator=mitigator,noise_backend=noise_backend, **kwargs)
+    loss = np.inner(coeffs, expectations)
+    end = timer()
+    print(f'Loss computed by VQE is {loss}, in {end - start} s.')
+    
+    if loss_filename is not None:
+        with open(loss_filename, 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow([loss])
+    
+    if params_filename is not None and parameters is not None:
+        with open(params_filename, 'a') as file:
+            writer = csv.writer(file)
+            writer.writerow(parameters)
+    return loss
+
+
+def run_vqe(n_qubits, coeffs, paulis, param_guess, budget, shots, backend, save_dir, loss_file, params_file, mode="device_execution", mitigator=None,noise_backend=None,**vqe_kwargs):
+    """
+    Run VQE instance. Uses skquant for optimization.
+    n_qubits (Int): Number of qubits in circuit.
+    coeffs (Iterable[Float]): Pauli coefficients in Hamiltonian.
+    paulis (Iterable[String]): Corresponding Pauli strings in Hamiltonian (same order as coeffs).
+    param_guess (Iterable[Float]): Initial guess for VQE parameters.
+    budget (Int): Max number of optimization iterations.
+    shots (Int): Number of VQE circuit execution shots.
+    mode (String): ["no_noisy_sim", "device_execution", "noisy_sim"].
+    backend (IBM backend): Can be simulator, fake backend or real backend; irrelevant with mode = "no_noisy_sim".
+    save_dir (String): Save directory.
+    loss_file (String): Name of save file for VQE loss/energy.
+    params_file (String): Name of save file for VQE parameters.
+    vqe_kwargs (Dict): Dictionary with additional keyword arguments for vqe() call.
+
+    Returns:
+    Tuple of energy estimate and optimized parameters.
+    """
+    # check right number of parameters given
+    _, num_params = efficientsu2_full(n_qubits, vqe_kwargs["ansatz_reps"])
+    if len(param_guess) == 0:
+        param_guess = [0] * num_params
+    assert len(param_guess) == num_params, f"Number of parameters given ({len(param_guess)}) does not match ansatz ({num_params})." 
+
+    bounds = np.array([[0, np.pi*2]]*num_params)  #>>> b=[[1,0]]*3  >>> b  [[1, 0], [1, 0], [1, 0]]  上下界
+    initial_point = np.array(param_guess)
+    #print('run_vqe') #debug,ac
+    vqe_result = minimize(
+        lambda c: vqe_zne(
+            n_qubits=n_qubits,
+            parameters=c, 
+            loss_filename=save_dir + "/" + loss_file,
+            params_filename=save_dir + "/" + params_file,
+            paulis=paulis, 
+            coeffs=coeffs,
+            shots=shots, 
+            backend=backend, 
+            mode=mode, 
+            mitigator=mitigator,
+            noise_backend=noise_backend,
+            **vqe_kwargs
+        ),  
+        initial_point, 
+        bounds, 
+        budget, 
+        method='imfil')
+    energy_vqe = vqe_result[0].optval
+    params_vqe = vqe_result[0].optpar
+    return energy_vqe, params_vqe
